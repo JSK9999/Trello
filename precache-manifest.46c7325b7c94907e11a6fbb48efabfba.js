3f715de96a19acbcebd8200ada7a3808e5070bf9
stepano9002 <suntrix3@gmail.com>
2020-02-22 00:19:28 +0900

    Updates

---
+++ precache-manifest.46c7325b7c94907e11a6fbb48efabfba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb1b60726ff67e83d0506790970a046",
+    "url": "/Trello/index.html"
+  },
+  {
+    "revision": "0d4d5cedc4131a665ce0",
+    "url": "/Trello/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f1bd0b4c82798499c534",
+    "url": "/Trello/static/js/2.e330ecf9.chunk.js"
+  },
+  {
+    "revision": "bea58cb82d8ee5349c7906e65d936513",
+    "url": "/Trello/static/js/2.e330ecf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4d5cedc4131a665ce0",
+    "url": "/Trello/static/js/main.17001b67.chunk.js"
+  },
+  {
+    "revision": "7578cd11cb3832cb44cf",
+    "url": "/Trello/static/js/runtime-main.0c43121f.js"
+  }
+]);
