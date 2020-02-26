b2ec899c0b7d207faf7e0b119901bd53ac87d0bc
stepano9002 <suntrix3@gmail.com>
2020-02-27 03:20:23 +0900

    색깔 조금 추가하고 새로고침해도 지워지지 않게 저장되게 했다.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
 import TrelloListContainer from "./container/TrelloListContainer";
-
+import styled from "styled-components";
+const Header = styled.div`
+  display: flex;
+  position: fixed;
+  z-index: 1;
+  height: 75px;
+  padding: 1rem;
+  font-size: 1.25rem;
+  text-align: justify;
+  margin: 0 auto;
+  color: #e6e6e6;
+  font-weight: bold;
+`;
 function App() {
-  return <TrelloListContainer />;
+  return (
+    <>
+      <TrelloListContainer />
+      <Header>
+        Trello의 리스트 와 카드 추가,수정,삭제 드래그 앤 드롭 기능이 있습니다.
+      </Header>
+    </>
+  );
 }
 export default App;
